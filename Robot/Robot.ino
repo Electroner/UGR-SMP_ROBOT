d0b33d7439c7449227a23163501beb4688f914dc
Carlos <clm2k1@gmail.com>
2022-06-27 19:43:58 +0200

    Finished code

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -19,14 +19,19 @@
 #define LINE_LEFT_PIN A2
 #define LINE_RIGHT_PIN A3
 
-#define WHITE_LINE_VALUE1 400	// Valor por el cual por encima consideramos que es blanco
-#define BLACK_LINE_VALUE1 100	// Valor por el cual por debajo consideramos que es negro
+#define WHITE_LINE_VALUE1 400 // Valor por el cual por encima consideramos que es blanco
+#define BLACK_LINE_VALUE1 100 // Valor por el cual por debajo consideramos que es negro
 
 #define WHITE_LINE_VALUE2 300
 #define BLACK_LINE_VALUE2 100
 
-
 #define DISTANCE_THRESHOLD 60
+
+//Convenio del Motor
+const struct MotorStruct{
+	int Derecha = 1;
+	int Izquierda = 2;
+}Motor;
 
 const int mpuAddress = 0x68;
 MPU6050 mpu(mpuAddress);
@@ -99,17 +104,16 @@ void get_sensor_data()
 
 void get_lines()
 {
-	lineFront = analogRead(LINE_FRONT_PIN)>= BLACK_LINE_VALUE1;
+	lineFront = analogRead(LINE_FRONT_PIN) >= BLACK_LINE_VALUE1;
 	lineBack = analogRead(LINE_BACK_PIN) >= BLACK_LINE_VALUE2;
 }
 
 void MotorHorario(int _motor, int _speed)
 {
-	// CONVENCION: MOTOR DERECHO = 1, MOTOR IZQUIERDO = 2
 	switch (_motor)
 	{
 	case 1:
-		analogWrite(MOTOR1_PIN1, (int)(((_speed / 100.0) * 255.0)*speed_factor));
+		analogWrite(MOTOR1_PIN1, (int)(((_speed / 100.0) * 255.0) * speed_factor));
 		analogWrite(MOTOR1_PIN2, LOW);
 		break;
 
@@ -122,12 +126,11 @@ void MotorHorario(int _motor, int _speed)
 
 void MotorAntihorario(int _motor, int _speed)
 {
-	// CONVENCION: MOTOR DERECHO = 1, MOTOR IZQUIERDO = 2
 	switch (_motor)
 	{
 	case 1:
 		analogWrite(MOTOR1_PIN1, LOW);
-		analogWrite(MOTOR1_PIN2, (int)(((_speed / 100.0) * 255.0)*speed_factor));
+		analogWrite(MOTOR1_PIN2, (int)(((_speed / 100.0) * 255.0) * speed_factor));
 		break;
 
 	case 2:
@@ -139,7 +142,6 @@ void MotorAntihorario(int _motor, int _speed)
 
 void MotorStop(int _motor)
 {
-	// CONVENCION: MOTOR DERECHO = 1, MOTOR IZQUIERDO = 2
 	switch (_motor)
 	{
 	case 1:
@@ -172,29 +174,14 @@ int readActivation()
 
 bool firsttime = true;
 
-int lastTime;
-
 void loop()
 {
-	//get_sensor_data();
+	// get_sensor_data();
 	get_distance();
 	get_lines();
 
 	if (readActivation())
 	{
-    if(firsttime){
-      delay(3000);
-      firsttime = false;
-    }else{
-    if(lineFront){
-        MotorHorario(1, speed);
-        MotorHorario(2, speed);
-    }else{
-        MotorStop(1);
-        MotorStop(2);
-    }
-    }
-		/*
 		if(firsttime){
 			delay(3000);
 			firsttime = false;
@@ -202,53 +189,47 @@ void loop()
 		else
 		{
 			// Comprobar que el enemigo esta delante y no nos salimos de la linea
-			if (distance <= DISTANCE_THRESHOLD && lineFront)
+			if (distance <= DISTANCE_THRESHOLD && lineFront && lineBack)
 			{
 				//Moverse hacia adelante
-				MotorHorario(1, speed);
-				MotorHorario(2, speed);
+				MotorHorario(Motor.Derecha, speed);
+				MotorHorario(Motor.Izquierda, speed);
 			}
-			if(distance > DISTANCE_THRESHOLD && lineBack)
-			{
-				//Moverse hacia atras
-				MotorAntihorario(1, speed);
-				MotorAntihorario(2, speed);
-			}
-			if(distance <= DISTANCE_THRESHOLD && !lineFront){
+			if(distance <= DISTANCE_THRESHOLD && !lineFront && lineBack){
 				//Parar
-				MotorStop(1);
-				MotorStop(2);
+				MotorStop(Motor.Derecha);
+				MotorStop(Motor.Izquierda);
 				//Hemos Ganado
 			}
-			if(distance > DISTANCE_THRESHOLD && !lineBack){
+
+			if(distance > DISTANCE_THRESHOLD && lineFront && lineBack)
+			{
+				//Moverse hacia atras
+				MotorAntihorario(Motor.Derecha, speed);
+				MotorAntihorario(Motor.Izquierda, speed);
+			}
+			if(distance > DISTANCE_THRESHOLD && lineFront && !lineBack){
 				//Parar
-				MotorStop(1);
-				MotorStop(2);
+				MotorStop(Motor.Derecha);
+				MotorStop(Motor.Izquierda);
 			}
 
-			// Comprobar que el enemigo esta a la izquierda y no nos salimos de la linea
-			if (distance > DISTANCE_THRESHOLD && lineFront && !lineBack)
-			{
-				//Moverse hacia la izquierda
-				MotorHorario(1, speed);
-				MotorAntihorario(2, speed);
+			if (distance <= DISTANCE_THRESHOLD && !lineBack){
+				//Moverse hacia adelante (Nos ganan)
+				MotorHorario(Motor.Derecha, speed);
+				MotorHorario(Motor.Izquierda, speed);
 			}
 		}
-		*/
 	}
 	else
 	{
-    MotorStop(1);
-    MotorStop(2);
-    Serial.print("FRONT: ");
-    Serial.print(analogRead(A0));
-    Serial.print("  BACK: ");
-    Serial.print(analogRead(A1));
+		MotorStop(1);
+		MotorStop(2);
+		Serial.print("FRONT: ");
+		Serial.print(analogRead(A0));
+		Serial.print("  BACK: ");
+		Serial.print(analogRead(A1));
 		Serial.print("  CM: ");
 		Serial.println(distance);
-		//Calcular el tiempo que tarda en ejecutarse el programa
-		Serial.print("  Tiempo: ");
-		lastTime = millis();
-		Serial.println(millis() - lastTime);
 	}
 }
